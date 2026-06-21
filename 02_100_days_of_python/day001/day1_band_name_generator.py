768c0b555bca6a02ba14181decd8f691dc8d6dbf
Luuk <luukpostma@outlook.com>
2026-06-21 14:53:50 +0200

    Changes

---
+++ 02_100_days_of_python/day001/day1_band_name_generator.py
@@ -0,0 +1,11 @@
+print("Welcome to the Band Name Generator")
+
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+# f-string manier.
+print(f"Your band name could be {city} {pet}")
+
+# Simpele manier
+# print("Your band name could be", city, pet)day1_band_name_generator.py
---
+++ 02_100_days_of_python/day002/00.py
@@ -0,0 +1,18 @@
+# Strings - Text tussen aanhalingstekens.
+name = "Luuk"
+
+# Strings
+other_name = "Stef"
+# Als je bijvoordbeeld de eerste letter van de variable wil printen.
+print(other_name[0])
+
+# Integers - Volledige nummers zonder decimalen.
+number = 22
+
+# Floats - Nummers met decimalen - kan tot 15/17 nummers.
+other_number = 7.22223
+print(other_number)
+
+# Boolean - true or false
+question = True
+other_question = False
